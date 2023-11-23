19e8321b0c7ce98bb3e8c436a2364f1db7481549
mrlabel001 <farbean.makini@student.gmail.com>
2023-11-23 10:49:31 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+    fibonacci_seq = [a, b]
+    while len(fibonacci_seq)<length:
+        a, b = b ,a + b
+        fibonacci_seq.append(b)
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        print(fibonacci_seq)
